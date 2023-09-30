e75aabae97bb1078e3887d4ff3cd247bb836dc93
nt0307e <nt0307e@iastate.edu>
2023-09-30 15:47:17 -0500

    chore: edited firebase

--- firebase.js
+++ firebase.js
@@ -1,12 +1,19 @@
-// Add Firebase SDKs (You get this from Firebase Console -> Project Settings)
-// Example:
-var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DB_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCr1y5hKS0XcP6aMHlSE8WF0NuF_rpn0N8",
+  authDomain: "hackisuv2tnt.firebaseapp.com",
+  projectId: "hackisuv2tnt",
+  storageBucket: "hackisuv2tnt.appspot.com",
+  messagingSenderId: "853060428664",
+  appId: "1:853060428664:web:49fbf2e9eeee7f01b95e8d",
+  measurementId: "G-871GG3L0CF"
 };
-firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
